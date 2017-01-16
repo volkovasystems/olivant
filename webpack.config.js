dca2f5689b3ab34253459b4a4f557e8c99881c42
johnlenon <johnlenonmaghanoy@gmail.com>
2017-01-16 17:46:52 +0800

    Improve build procedure.

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const webpack = require( "webpack" );
+const IgnorePlugin = webpack.IgnorePlugin;
+const ResolverPlugin = webpack.ResolverPlugin;
+const DirectoryDescriptionFilePlugin = ResolverPlugin.DirectoryDescriptionFilePlugin;
+const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+
+module.exports = {
+	"entry": "./olivant.support.js",
+	"resolve": {
+		"modulesDirectories": [ "bower_components", "node_modules" ]
+	},
+	"module": {
+		"preLoaders": [
+			{
+				"test": /\.support\.js$/,
+				"loader": "source-map-loader"
+			}
+		]
+	},
+	"output": {
+		"library": "olivant",
+		"libraryTarget": "umd",
+		"filename": "olivant.deploy.js"
+	},
+	"plugins": [
+		new IgnorePlugin(  ),
+		new ResolverPlugin( new DirectoryDescriptionFilePlugin( "bower.json", [ "support" ] ) ),
+		new ResolverPlugin( new DirectoryDescriptionFilePlugin( "package.json", [ "main" ] ) ),
+		new ResolverPlugin( new DirectoryDescriptionFilePlugin( ".bower.json", [ "main" ] ) ),
+		new UglifyJsPlugin( {
+			"compress": {
+				"keep_fargs": true,
+				"keep_fnames": true,
+				"warnings": false
+			},
+			"comments": false,
+			"sourceMap": true,
+			"mangle": false
+		} )
+	],
+	"devtool": "#inline-source-map"
+};
