8ce46fccb0c7f9f5ccc816de9f35eb0ef4b2de6e
johnlenon <johnlenonmaghanoy@gmail.com>
2017-05-18 16:28:13 +0800

    Updated test.

--- olivant-test.js
+++ olivant-test.js
@@ -8,8 +8,16 @@ Prompt( "empty title", { "title": "" } ).prompt( "cannot set title" );
 
 Uncertain( "empty store", { "store": "" } ).prompt( "cannot set title" );
 
+Record( "existing store", { "store": "" } ).prompt( "cannot set store" );
+
+Echo( "creating merchant", { "store": "" } ).prompt( "loading" );
+
 Success( "Merchant created", { "name": "biyaheroes" } ).prompt( "created" );
 
+Failed( "Unable to create merchant", { "name": "biyaheroes" } ).prompt( "rolling back" );
+
 Issue( "empty payment", { } ).prompt( "cannot resolve payment" );
+
+Bug( "merchant invalid data", { "sadsdsd": "dfdsfd" } ).prompt( "unsuccessful" );
 
 Fatal( "nothing is permanent", new Error( "this is an error" ) );
