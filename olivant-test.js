cf5eac6009db4ee014a0409f848e0416baf08c1c
johnlenon <johnlenonmaghanoy@gmail.com>
2017-05-18 14:14:42 +0800

    Save changes.

--- olivant-test.js
+++ olivant-test.js
@@ -4,6 +4,12 @@ require( "./index.js" );
 
 Warning( "empty name", { "name": "" } ).prompt( "cannot set name" );
 
-Fatal( "nothing is permanent", new Error( "this is an error" ) );
-
 Prompt( "empty title", { "title": "" } ).prompt( "cannot set title" );
+
+Uncertain( "empty store", { "store": "" } ).prompt( "cannot set title" );
+
+Success( "Merchant created", { "name": "biyaheroes" } ).prompt( "created" );
+
+Issue( "empty payment", { } ).prompt( "cannot resolve payment" );
+
+Fatal( "nothing is permanent", new Error( "this is an error" ) );
